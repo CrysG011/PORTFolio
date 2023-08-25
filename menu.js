1f425c7206900816d445193a7a01a3caf20c2e3f
crystina <crystinagonz@gmail.com>
2023-08-25 00:34:06 -0300

    incorporo typescript

--- menu.js
+++ menu.js
@@ -1,76 +1,59 @@
-const button = document.querySelector('.button');
-const nav = document.querySelector('.nav');
-button.addEventListener('click', () => {
-  nav.classList.toggle('active')
-})
-
-const toggleButton = document.getElementById('toggleButton');
-const openCubes = Array.from(document.querySelectorAll("#open-cubes rect"));
-const closeCubes = Array.from(document.querySelectorAll("#close-cubes rect"));
-let locked = false;
-
-const openEffect = (cubes) => {
-  cubes.forEach((element, index) => {
-    setTimeout(() => {
-      element.classList.remove("spin-in");
-      element.classList.add("spin-out");
-    }, index * 10);
-  });
+var button = document.querySelector('.button');
+var nav = document.querySelector('.nav');
+button.addEventListener('click', function () {
+    nav.classList.toggle('active');
+});
+var toggleButton = document.getElementById('toggleButton');
+var openCubes = Array.from(document.querySelectorAll("#open-cubes rect"));
+var closeCubes = Array.from(document.querySelectorAll("#close-cubes rect"));
+var locked = false;
+var openEffect = function (cubes) {
+    cubes.forEach(function (element, index) {
+        setTimeout(function () {
+            element.classList.remove("spin-in");
+            element.classList.add("spin-out");
+        }, index * 10);
+    });
 };
-
-const closeEffect = (cubes) => {
-  cubes.forEach((element, index) => {
-    setTimeout(() => {
-      element.classList.remove("spin-out");
-      element.classList.add("spin-in");
-    }, index * 10);
-  });
+var closeEffect = function (cubes) {
+    cubes.forEach(function (element, index) {
+        setTimeout(function () {
+            element.classList.remove("spin-out");
+            element.classList.add("spin-in");
+        }, index * 10);
+    });
 };
-
-button.addEventListener("click", (e) => {
-  if (!locked) {
-    locked = true;
-    button.classList.toggle("active");
-    const length = openCubes.length;
-
-    if (button.classList.contains("active")) {
-      openEffect(openCubes);
-      closeEffect(closeCubes);
-    } else {
-      closeEffect(openCubes);
-      openEffect(closeCubes);
+button.addEventListener("click", function () {
+    if (!locked) {
+        locked = true;
+        button.classList.toggle("active");
+        var length_1 = openCubes.length;
+        if (button.classList.contains("active")) {
+            openEffect(openCubes);
+            closeEffect(closeCubes);
+        }
+        else {
+            closeEffect(openCubes);
+            openEffect(closeCubes);
+        }
+        setTimeout(function () {
+            locked = false;
+        }, 1300 + length_1 * 10);
     }
-
-    setTimeout(() => {
-      locked = false;
-    }, 1300 + length * 10);
-  }
 });
-
-
-const textarea = document.getElementById('mensaje');
-const maxLength = 300;
-
+var textarea = document.getElementById('mensaje');
+var maxLength = 300;
 textarea.addEventListener('input', function () {
-  if (textarea.value.length > maxLength) {
-    textarea.value = textarea.value.slice(0, maxLength);
-  }
+    if (textarea.value.length > maxLength) {
+        textarea.value = textarea.value.slice(0, maxLength);
+    }
 });
-
-const submitButton = document.getElementById('submitButton');
-
+var submitButton = document.getElementById('submitButton');
 submitButton.addEventListener('click', function (event) {
-  event.preventDefault(); // Evita que se envíe el formulario
-
-  // Verifica si los campos requeridos están completos
-  const requiredFields = document.querySelectorAll('[required]');
-  const areFieldsCompleted = Array.from(requiredFields).every(field => field.value.trim() !== '');
-
-  if (areFieldsCompleted) {
-    alert('¡GRACIAS por visitar mi sitio!\nMENSAJE ENVIADO 😊');
-  }
+    event.preventDefault();
+    var requiredFields = document.querySelectorAll('[required]');
+    var areFieldsCompleted = Array.from(requiredFields).every(function (field) { return field.value.trim() !== ''; });
+    if (areFieldsCompleted) {
+        alert('¡GRACIAS por visitar mi sitio!\nMENSAJE ENVIADO 😊');
+    }
 });
-
-
-
-
---
+++ menu.ts
@@ -0,0 +1,70 @@
+const button = document.querySelector('.button') as HTMLButtonElement;
+const nav = document.querySelector('.nav') as HTMLDivElement;
+button.addEventListener('click', () => {
+  nav.classList.toggle('active');
+});
+
+const toggleButton = document.getElementById('toggleButton') as HTMLButtonElement;
+const openCubes = Array.from(document.querySelectorAll("#open-cubes rect"));
+const closeCubes = Array.from(document.querySelectorAll("#close-cubes rect"));
+let locked = false;
+
+const openEffect = (cubes: HTMLElement[]) => {
+  cubes.forEach((element, index) => {
+    setTimeout(() => {
+      element.classList.remove("spin-in");
+      element.classList.add("spin-out");
+    }, index * 10);
+  });
+};
+
+const closeEffect = (cubes: HTMLElement[]) => {
+  cubes.forEach((element, index) => {
+    setTimeout(() => {
+      element.classList.remove("spin-out");
+      element.classList.add("spin-in");
+    }, index * 10);
+  });
+};
+
+button.addEventListener("click", () => {
+  if (!locked) {
+    locked = true;
+    button.classList.toggle("active");
+    const length = openCubes.length;
+
+    if (button.classList.contains("active")) {
+      openEffect(openCubes);
+      closeEffect(closeCubes);
+    } else {
+      closeEffect(openCubes);
+      openEffect(closeCubes);
+    }
+
+    setTimeout(() => {
+      locked = false;
+    }, 1300 + length * 10);
+  }
+});
+
+const textarea = document.getElementById('mensaje') as HTMLTextAreaElement;
+const maxLength = 300;
+
+textarea.addEventListener('input', () => {
+  if (textarea.value.length > maxLength) {
+    textarea.value = textarea.value.slice(0, maxLength);
+  }
+});
+
+const submitButton = document.getElementById('submitButton') as HTMLButtonElement;
+
+submitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const requiredFields = document.querySelectorAll<HTMLInputElement>('[required]');
+  const areFieldsCompleted = Array.from(requiredFields).every((field: HTMLInputElement) => field.value.trim() !== '');
+
+  if (areFieldsCompleted) {
+    alert('¡GRACIAS por visitar mi sitio!\nMENSAJE ENVIADO 😊');
+  }
+});
